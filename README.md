8458dbeabfa39caad2a8c9d92e73f1d617478b8e
masafy <masafy.masato@gmail.com>
2023-02-19 20:56:23 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# github_sample_proj
+久々すぎて使い方忘れたまんじ
---
+++ sample.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+
+def main():
+    url = "https://news.yahoo.co.jp/" 
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    rs = soup.find_all(href=re.compile('news.yahoo.co.jp/pickup'))
+    df = pd.DataFrame(index=[], columns=['text', 'link'])
+    for i in rs:
+        text = i.getText()
+        link = i.attrs['href']
+        index = pd.Series([text, link], index=df.columns)
+        df = df.append(index, ignore_index=True)
+    print(df)
+
+if __name__=="__main__":
+    main()
